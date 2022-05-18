d5c33527bacfe798f0686719ab3766aefeeaeeb8
{olenak} <{olenak@default-value.com}>
2022-05-18 12:10:39 +0200

    #6 DB Scema Form Key: create form and move it to the tab for local site

---
+++ app/code/OlenaK/RegularCustomer/Controller/Request/Form.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OlenaK\RegularCustomer\Controller\Request;
+
+class Form
+{
+
+}
---
+++ app/code/OlenaK/RegularCustomer/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'OlenaK_RegularCustomer',
+    __DIR__
+);
