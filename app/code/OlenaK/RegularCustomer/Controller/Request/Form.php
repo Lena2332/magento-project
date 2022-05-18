e5e9a794a5e5967453dfaef097d50518510cf1d9
{olenak} <{olenak@default-value.com}>
2022-05-18 16:07:49 +0200

    #6 DB Scema Form Key: create page form

--- app/code/OlenaK/RegularCustomer/Controller/Request/Form.php
+++ app/code/OlenaK/RegularCustomer/Controller/Request/Form.php
@@ -4,7 +4,28 @@ declare(strict_types=1);
 
 namespace OlenaK\RegularCustomer\Controller\Request;
 
-class Form
-{
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\Page;
 
+class Form implements HttpGetActionInterface
+{
+    private \Magento\Framework\View\Result\PageFactory $pageFactory;
+
+    /**
+     * @param \Magento\Framework\View\Result\PageFactory $pageFactory
+     */
+    public function __construct(\Magento\Framework\View\Result\PageFactory $pageFactory)
+    {
+        $this->pageFactory = $pageFactory;
+    }
+
+    /**
+     * Page result demo: https://olena-kupriiets-magento.local/olenak-cms/lesson/cmscontroller
+     *
+     * @return Page
+     */
+    public function execute(): Page
+    {
+        return $this->pageFactory->create();
+    }
 }
