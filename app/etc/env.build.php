b9bf34c351e446951a32fbac631c72ea3699846c
{olenak} <{olenak@default-value.com}>
2022-06-09 12:25:25 +0200

    #9 Code delivery: add env.build

---
+++ app/etc/env.build.php
@@ -0,0 +1,104 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'remote_storage' => [
+        'driver' => 'file'
+    ],
+    'queue' => [
+        'consumers_wait_for_messages' => 1
+    ],
+    'crypt' => [
+        'key' => '94dfef96f44420c7ab1fc6ed8cc23fbe'
+    ],
+    'db' => [
+        'table_prefix' => 'm2_',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'olena_kupriiets_magento_build',
+                'username' => 'olena_kupriiets_build_user',
+                'password' => 'Gfj@i23l-OSz:s325:v3+7@cdkswsdp',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1',
+                'driver_options' => [
+                    1014 => false
+                ]
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'production',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '69d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '69d_'
+            ]
+        ],
+        'allow_parallel_generation' => false
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => ''
+        ]
+    ],
+    'directories' => [
+        'document_root_is_pub' => true
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'olena-kupriiets-magento-local.allbugs.info'
+    ],
+    'install' => [
+        'date' => 'Thu, 07 Oct 2021 15:54:23 +0000'
+    ],
+    'system' => [
+        'default' => [
+            'system' => [
+                'smtp' => [
+                    'disable' => 1
+                ],
+                'full_page_cache' => [
+                    'caching_application' => 1
+                ]
+            ],
+            'catalog' => [
+                'search' => [
+                    'elasticsearch7_index_prefix' => 'build'
+                ]
+            ]
+        ],
+    ]
+];
+
