91555dd87c99fb225acf4502ba6539b1ee21a9c5
{olenak} <{olenak@default-value.com}>
2022-06-17 09:20:06 +0200

    #16 JS Layout: revrite showing form blocks to components

--- app/code/OlenaK/RegularCustomer/view/frontend/requirejs-config.js
+++ app/code/OlenaK/RegularCustomer/view/frontend/requirejs-config.js
@@ -3,6 +3,7 @@ var config = {
         '*': {
             'OlenaK_RegularCustomer_form': 'OlenaK_RegularCustomer/js/view/form',
             'OlenaK_RegularCustomer_formOpenButton': 'OlenaK_RegularCustomer/js/view/form-open-button',
+            'OlenaK_RegularCustomer_formTitle': 'OlenaK_RegularCustomer/js/view/form-title',
             'OlenaK_RegularCustomer_customAjax': 'OlenaK_RegularCustomer/js/action/custom-ajax',
             'OlenaK_RegularCustomer_formSubmitRestriction': 'OlenaK_RegularCustomer/js/model/form-submit-restrictions'
         }
---
+++ app/code/OlenaK/RegularCustomer/view/frontend/web/js/view/form-title.js
@@ -0,0 +1,12 @@
+define([
+    'jquery',
+    'uiComponent'
+], function ($, Component) {
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'OlenaK_RegularCustomer/form-title'
+        }
+    });
+});
