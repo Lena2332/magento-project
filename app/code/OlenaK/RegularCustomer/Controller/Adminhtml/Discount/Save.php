616cec7788cc5e6ac80182c044113d27c8f72d1a
{olenak} <{olenak@default-value.com}>
2022-06-22 16:09:27 +0200

    #19 Admin Panel Forms: implement productId and customerId validation

--- app/code/OlenaK/RegularCustomer/Controller/Adminhtml/Discount/Save.php
+++ app/code/OlenaK/RegularCustomer/Controller/Adminhtml/Discount/Save.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace OlenaK\RegularCustomer\Controller\Adminhtml\Discount;
 
+use Magento\Catalog\Model\ResourceModel\Product\Collection as ProductCollection;
+use Magento\Customer\Model\ResourceModel\Customer\Collection as CustomerCollection;
 use OlenaK\RegularCustomer\Model\Authorization;
 use Magento\Framework\Controller\ResultInterface;
 
@@ -22,19 +24,35 @@ class Save extends \Magento\Backend\App\Action implements \Magento\Framework\App
     private \OlenaK\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource;
 
     /**
+     * @var \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     */
+    private \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory;
+
+    /**
+     * @var \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory $customerCollectionFactory
+     */
+    private \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory $customerCollectionFactory;
+
+    /**
      * Save constructor.
      * @param \OlenaK\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory
      * @param \OlenaK\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource
      * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     * @param \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory $customerCollectionFactory
      */
     public function __construct(
         \OlenaK\RegularCustomer\Model\DiscountRequestFactory $discountRequestFactory,
         \OlenaK\RegularCustomer\Model\ResourceModel\DiscountRequest $discountRequestResource,
-        \Magento\Backend\App\Action\Context $context
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        \Magento\Customer\Model\ResourceModel\Customer\CollectionFactory $customerCollectionFactory
     ) {
         parent::__construct($context);
         $this->discountRequestFactory = $discountRequestFactory;
         $this->discountRequestResource = $discountRequestResource;
+        $this->productCollectionFactory = $productCollectionFactory;
+        $this->customerCollectionFactory = $customerCollectionFactory;
     }
 
     /**
@@ -58,7 +76,46 @@ class Save extends \Magento\Backend\App\Action implements \Magento\Framework\App
             return $resultRedirect->setPath('*/*/');
         }
 
-        // @TODO (practice): validate Customer ID and Product ID, show error message in case they are invalid
+        //Product validation
+        $productFormId = (int) $request->getParam('product_id');
+
+        /** @var ProductCollection $productCollection */
+        $productCollection = $this->productCollectionFactory->create();
+        $productCollection->addIdFilter($productFormId)->setPageSize(1);
+        $product = $productCollection->getFirstItem();
+        $productId = (int) $product->getId();
+
+        if (!$productId) {
+            $this->messageManager->addErrorMessage(__('Product with id %1 does not exist.', $productFormId));
+
+            return $resultRedirect->setPath(
+                '*/*/edit',
+                [
+                    'request_id' => $discountRequest->getId()
+                ]
+            );
+        }
+
+        //Customer validation
+        $customerFormId = (int) $request->getParam('customer_id');
+
+        /** @var CustomerCollection $customerCollection */
+        $customerCollection = $this->customerCollectionFactory->create();
+        $customerCollection->addFilter('entity_id', $customerFormId)->setPageSize(1);
+        $customer = $customerCollection->getFirstItem();
+        $customerId = (int) $customer->getId();
+
+        if (!$customerId) {
+            $this->messageManager->addErrorMessage(__('Customer with id %1 does not exist.', $customerFormId));
+
+            return $resultRedirect->setPath(
+                '*/*/edit',
+                [
+                    'request_id' => $discountRequest->getId()
+                ]
+            );
+        }
+
         $discountRequest->setProductId(((int) $request->getParam('product_id')) ?: null)
             ->setCustomerId(((int) $request->getParam('customer_id')) ? : null)
             ->setName($request->getParam('name'))
