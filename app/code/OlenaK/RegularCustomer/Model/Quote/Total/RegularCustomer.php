ba2e4ae29017645365093acbdcba45d48199d9d7
{olenak} <{olenak@default-value.com}>
2022-06-30 14:26:54 +0200

    #22 Order Total: implement applying discount only for asked products

--- app/code/OlenaK/RegularCustomer/Model/Quote/Total/RegularCustomer.php
+++ app/code/OlenaK/RegularCustomer/Model/Quote/Total/RegularCustomer.php
@@ -5,12 +5,35 @@ declare(strict_types=1);
 namespace OlenaK\RegularCustomer\Model\Quote\Total;
 
 use Magento\Quote\Model\Quote\Address\Total\AbstractTotal;
+use OlenaK\RegularCustomer\Model\DiscountRequest;
+use OlenaK\RegularCustomer\Model\ResourceModel\Collection\CollectionFactory as DiscountRequestCollectionFactory;
 
 class RegularCustomer extends \Magento\Quote\Model\Quote\Address\Total\AbstractTotal
 {
     public const DISCOUNT_PERCENT = 0.05;
 
     public const TOTAL_CODE = 'regular_customer';
+
+    private $productRequestIds = [];
+
+    /**
+     * @var \Magento\Customer\Model\Session $customerSession
+     */
+    private \Magento\Customer\Model\Session $customerSession;
+
+    /**
+     * @var DiscountRequestCollectionFactory $discountRequestCollectionFactory
+     */
+    private DiscountRequestCollectionFactory $discountRequestCollectionFactory;
+
+    public function __construct(
+        \Magento\Customer\Model\Session $customerSession,
+        DiscountRequestCollectionFactory $discountRequestCollectionFactory
+    )
+    {
+        $this->customerSession = $customerSession;
+        $this->discountRequestCollectionFactory = $discountRequestCollectionFactory;
+    }
 
     /**
      * @param \Magento\Quote\Model\Quote $quote
@@ -24,13 +47,37 @@ class RegularCustomer extends \Magento\Quote\Model\Quote\Address\Total\AbstractT
         \Magento\Quote\Model\Quote\Address\Total $total
     ): AbstractTotal {
         parent::collect($quote, $shippingAssignment, $total);
-        $regularCustomer = -($total->getSubtotal() * self::DISCOUNT_PERCENT);
-        $baseRegularCustomer = -($total->getBaseSubtotal() * self::DISCOUNT_PERCENT);
 
-        $total->addTotalAmount(self::TOTAL_CODE, $regularCustomer);
-        $total->addBaseTotalAmount(self::TOTAL_CODE, $baseRegularCustomer);
-        $quote->setData(self::TOTAL_CODE, $regularCustomer);
-        $quote->setData('base_' . self::TOTAL_CODE, $baseRegularCustomer);
+        //Get from session productIds for request
+        $this->productRequestIds = $this->getProductRequestIds();
+
+        //Calculate our discount
+        $productsInCart = $quote->getAllItems();
+        $regularCustomerDiscount = 0;
+        $baseRegularCustomerDiscount = 0;
+        foreach ($productsInCart as $product) {
+            $productId = (int) $product->getProduct()->getId();
+
+            $baseProductPrice = ($product->getParentItem()) ?
+                $product->getParentItem()->getBaseCalculationPrice() :
+                $product->getBaseCalculationPrice();
+
+            $productPrice = ($product->getParentItem()) ?
+                $product->getParentItem()->getCalculationPrice() :
+                $product->getCalculationPrice();
+
+            $quantity = $product->getTotalQty();
+
+            if (in_array($productId, $this->productRequestIds)) {
+                $regularCustomerDiscount += ($productPrice * self::DISCOUNT_PERCENT) * $quantity;
+                $baseRegularCustomerDiscount += ($baseProductPrice * self::DISCOUNT_PERCENT) * $quantity;
+            }
+        }
+
+        $total->addTotalAmount(self::TOTAL_CODE, -$regularCustomerDiscount);
+        $total->addBaseTotalAmount(self::TOTAL_CODE, -$baseRegularCustomerDiscount);
+        $quote->setData(self::TOTAL_CODE, -$regularCustomerDiscount);
+        $quote->setData('base_' . self::TOTAL_CODE, -$baseRegularCustomerDiscount);
 
         return $this;
     }
@@ -60,5 +107,28 @@ class RegularCustomer extends \Magento\Quote\Model\Quote\Address\Total\AbstractT
     public function getLabel()
     {
         return __('Regular Customer');
+    }
+
+    /**
+     * @return array
+     */
+    private function getProductRequestIds(): array
+    {
+        if ($this->customerSession->isLoggedIn()) {
+            $customerId = $this->customerSession->getCustomerId();
+
+            /** @var DiscountRequest $discountRequest */
+            $discountRequestCollection = $this->discountRequestCollectionFactory->create();
+            $productRequestIds = $discountRequestCollection->addFieldToFilter('status',
+                ['eq' => DiscountRequest::STATUS_APPROVED]
+            )->addFieldToFilter('customer_id',
+                ['eq' => $customerId]
+            )->getColumnValues('product_id');
+            $productRequestIds = array_map('intval', $productRequestIds);
+        } else {
+            $productRequestIds = $this->customerSession->getDiscountRequestProductIds();
+        }
+
+        return $productRequestIds;
     }
 }
